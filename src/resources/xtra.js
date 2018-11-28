41e0eaf1c23fc56771ae5470455f850b438f70d4
mamgeorge <mamgeorge@yahoo.com>
2018-11-27 21:09:00 -0500

    updated colors

--- src/resources/xtra.js
+++ src/resources/xtra.js
@@ -1,10 +1,45 @@
+//
+const REDB = '\033[41m';
+const REDF = '\033[91m';
+const YELF = '\033[93m';
+const CYAN = '\033[96m';
+const GRNB = '\033[42m';
+const GRNF = '\033[92m';
+const BLUF = '\033[34m';
+const PRPL = '\033[95m';
+const GRYF = '\033[90m';
+const GRYB = '\033[100m';
+const WHTB = '\033[107m';
+const BLNK = '\033[0m';
+const DLMT = '██████████' ;
 
-function calcs( lister ) {
+exports.showColors = function( ) {
+	//
+	var txtLine = '' ; 
+	for( var ictr = 29 ; ictr <= 107 ; ictr++ ) { 
+		if( ictr >= 48 && ictr < 89 ) { continue; }
+		if( ictr == 89 ) { txtLine += '\n'; }
+		txtLine += '\033[' + ictr + 'm[' + ictr + ']' + BLNK ; 
+	} ; 
+	return txtLine ;
+}
+
+exports.testResults = function( results , expects ) {
+	//
+	let txtLine = '' ;
+	if ( results === expects )
+	{ txtLine = GRNF + DLMT + ' PASS! ' + DLMT + BLNK + '\n' ; } else
+	{ txtLine = REDF + DLMT + ' FAIL! ' + DLMT + BLNK
+		 + ' results: ' + results + ' , expects: ' + expects + '\n' ; }
+	return txtLine;
+}
+
+exports.calcRegression = function( lister ) {
 	//
 	if (lister === undefined || lister === null || lister.length === 0) {
 		lister = { 'points': [[13, 1], [9, 2], [7, 3], [14, 4], [7, 5], [9, 6], [2, 7]] };
 	};
-	let txtLine = '';
+	let txtLine = WHTB;
 	let Nval = lister.points.length;
 	let avgxs = 0 , avgys = 0
 	let sumxs = 0 , sumx2s = 0 , sumys = 0 , sumy2s = 0 , sumxys = 0;
@@ -29,24 +64,25 @@ function calcs( lister ) {
 	let intercept = ( sumys - ( slope * sumxs ) ) / Nval ;
 	let corr = XYVL / SXYVL ;
 	//
-	txtLine += '-------------------\n';
+	txtLine += BLNK;
+	txtLine += REDF + '-------------------\n';
 	txtLine += 'Σx: ' + sumxs + '\t\t Σy : ' + sumys + '\n' ;
 	txtLine += 'Σx²: '+ sumx2s+ '\t\t Σy²: ' + sumy2s+ '\n' ;
 	txtLine += 'αx: ' + avgxs + '\t\t αy : ' + avgys + '\n' ;
-	txtLine += 'n : ' + Nval  + '\t\t Σxy: ' + sumxys + '\n';
-	txtLine += '-------------------\n' ;
+	txtLine += 'n : ' + Nval  + '\t\t Σxy: ' + sumxys + '\n' + BLNK ;
+	txtLine += BLUF + '-------------------\n' ;
 	txtLine += 'XVAL: N*Σx² - (Σx)² \t\t' + XVAL + '\n' ;
 	txtLine += 'YVAL: N*Σy² - (Σy)² \t\t' + YVAL + '\n' ;
 	txtLine += 'XYVL: NΣxy - (Σx*Σy) \t\t' + XYVL + '\n' ;
 	txtLine += 'σX = √[ XVAL / N(N-1) ] \t' + SDVX + '\n' ;
 	txtLine += 'σy = √[ YVAL / N(N-1) ] \t' + SDVY + '\n' ;
-	txtLine += 'SXYVL: √[ XVAL * YVAL ] \t\t' + SXYVL + '\n' ;
-	txtLine += '-------------------\n' ;
+	txtLine += 'SXYVL: √[ XVAL * YVAL ] \t' + SXYVL + '\n' + BLNK ;
+	txtLine += GRNF + '-------------------\n' ;
 	txtLine += 'm: XYVL / XVAL: \t\t' + slope + '\t = r*(σy/σX) = ' + ( corr * ( SDVY/SDVX ) ) + '\n' ;
-	txtLine += 'b: [ Σy - (m*Σx) ]/N: \t\t' + intercept + '\n' ;
-	txtLine += '-------------------\n' ;	
+	txtLine += 'b: [ Σy - (m*Σx) ]/N: \t\t' + intercept + '\n' + BLNK ;
+	txtLine += YELF + '-------------------\n' ;	
 	txtLine += 'r: XYVL / √[XVAL*YVAL ]:\t' + corr + '\n' ;
-	txtLine += 'y = m: ( ' + slope + ' ) x + b: ( ' + intercept + ' )\n' ;
+	txtLine += 'y = m: ( ' + slope + ' ) x + b: ( ' + intercept + ' )\n' + BLNK ;
 	console.log( txtLine ) ; 
 	//
 	var stats = { 
@@ -55,6 +91,9 @@ function calcs( lister ) {
 		"line" : { "slope": slope, "intercept": intercept , "corr": corr  } , 
 		lister
 	};
+	//	
+	console.log( CYAN + 'stats: ' + JSON.stringify( stats ) + BLNK + '\n' );
+	console.log( PRPL + 'corr: '  + stats.line.corr + BLNK + '\n' );
+	//
 	return stats;
 }
-module.exports = { calcs };
--- tests/sample.js
+++ tests/sample.js
@@ -1,41 +1,12 @@
 
-const BLNK = '\033[0m';
-const BLUE = '\033[34m';
-const PRPL = '\033[95m';
-const GRNB = '\033[42m';
-const GRNF = '\033[92m';
-const REDB = '\033[41m';
-const REDF = '\033[91m';
-const YELF = '\033[93m';
-const CYAN = '\033[96m';
-const GRYF = '\033[90m';
-const DLMT = '          ' ;
-
-function testor( expects , results ) {
-	let evaluation = '' ;
-	if ( expects === results )
-	{ evaluation = GRNB + DLMT + BLNK + ' ' + GRNF + 'PASS!' + BLNK + ' ' + GRNB + DLMT + BLNK + '\n' ; } else
-	{ evaluation = REDB + DLMT + BLNK + ' ' + REDF + 'FAIL!' + BLNK + ' ' + REDB + DLMT + BLNK
-		 + ' expects: ' + expects + ' , results: ' + results + '\n' ; }
-	return evaluation;
-}	
-
+const xtra = require( './../src/resources/xtra.js' );
 
 // test calcs
-const { calcs } = require( './../src/resources/xtra' );
 const expects = -0.6318176543806739;
-
-let lister = { 'points': [ [ -3 , -2 ] , [ 5 , 4 ] ] }; // 0,0
-lister = null;
-
-let stats = calcs( lister );
+let lister = { 'points': [ [ -3 , -2 ] , [ 5 , 4 ] ] }; lister = null;
+//
+let stats = xtra.calcRegression( lister );
 let results = stats.line.corr;
-
-console.log( '\t' + CYAN + 'stats: ' + BLNK + GRYF + JSON.stringify( stats ) + BLNK + '\n' );
-console.log( '\t' + PRPL + 'corr: ' + BLNK + YELF + results + BLNK + '\n' );
-
-console.log( testor( expects , null ) );
-console.log( testor( expects , results ) );
-
-// colors
-// var qc = '' ; for( i =30 ; i < 107 ; i++ ) { qc += '\033[' + i + 'm[' + i + ']\033[0m' ; } ; console.log( qc );
+//
+// console.log( xtra.showColors( ) );
+console.log( xtra.testResults( results , expects ) );
