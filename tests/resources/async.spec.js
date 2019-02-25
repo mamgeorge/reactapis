4fe33f4dfff47b724330bf704653e5f288575f18
mamgeorge <mamgeorge@yahoo.com>
2019-02-25 07:32:40 -0500

    error messages

--- tests/resources/async.spec.js
+++ tests/resources/async.spec.js
@@ -9,32 +9,43 @@ describe( 'The async tests', () => {
 		//
 		it.skip( 'should return found user data by id 0 ( fails without help )', () => {
 			result = findUserById( 1 )
-			console.log( 'result0: ', result )
+			// console.log( 'result0: ', result )
 			assert.equal( result.user.name, 'mamgeorge' )
 			// result:  Promise { <pending> } 
 		} )
 		it( 'should return found user data by id 1 ( using the done callback )', (done) => {
 			findUserById( 1 ).then( result => {
-				console.log( '### result1: ', result )
+				// console.log( '### result1: ', result )
 				assert.equal( result.user.name, 'mamgeorge' )
 				done();
 			} )
 		} )
 		it( 'should return found user data by id 2 ( returning the promise )', () => {
 			return findUserById( 1 ).then( result => {
-				console.log( '### result2: ', result )
+				// console.log( '### result2: ', result )
 				assert.equal( result.user.name, 'mamgeorge' )
 			} )
 		} )	
-		it( 'should throw an error if id not found 3', () => {
-			return findUserById( 90393 ).catch( error => {
-				// console.log( '### result3: ', result )
-				assert.equal( error.message, 'User with id: 90393 was not found.' )
-			} )
-		} )
-		it( 'should return user found by id (Using async function)', async () => {
+		it( 'should return found user data by id 3 (Using await function)', async () => {
 			const result = await findUserById( 1 )
 			assert.equal( result.user.name, 'mamgeorge' )
+		} )
+		it( 'should return found user message', async () => {
+			const result = await findUserById( 1 )
+			assert.equal( result.message, 'User found successfully.' )
+		} )
+		it( 'should return reject user message', async () => {
+			const id = 90909;
+			return findUserById( id ).catch( error => {
+				assert.equal( error, `Error: User with id: ${ id } was not found.` )
+			} )
+		} )	
+		it( 'should throw an error if id not found 3', () => {
+			const id = 90909;
+			return findUserById( id ).catch( error => {
+				// console.log( '### result3: ', result )
+				assert.equal( error.message, `User with id: ${id} was not found.` )
+			} )
 		} )
 	} )
 
